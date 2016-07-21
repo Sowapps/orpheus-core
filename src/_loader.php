a5ed512090d5b8c89bb181fb3f4f2f91faebcf78
Loenix <florent@sowapps.com>
2016-07-21 15:45:43 +0200

    Do not load in non orpheus environment

--- src/_loader.php
+++ src/_loader.php
@@ -3,24 +3,12 @@
  * Loader File for the core sources
  */
 
+if( !defined('ORPHEUSPATH') ) {
+	// Do not load in a non-orpheus environment
+	return;
+}
+
 require_once 'core.php';
 require_once 'hooks.php';
 require_once 'validators.php';
 require_once 'exceptions.php';
-
-// Core
-// require_once pathOf(LIBSDIR.'core/core.php');
-
-// Important
-// addAutoload('ConfigCore',						'core/ConfigCore.php');
-
-// require_once pathOf(LIBSDIR.'core/hooks.php');
-// require_once pathOf(LIBSDIR.'core/validators.php');
-
-// addAutoload('UserException',					'core/userexception_class.php');
-// addAutoload('NotFoundException',				'core/userexception_class.php');
-// addAutoload('ForbiddenException',				'core/userexception_class.php');
-// addAutoload('OperationCancelledException',		'core/userexception_class.php');
-// addAutoload('SQLException',						'core/sqlexception_class.php');
-// addAutoload('FormToken',						'core/FormToken');
-// addAutoload('SlugGenerator',					'core/SlugGenerator');
